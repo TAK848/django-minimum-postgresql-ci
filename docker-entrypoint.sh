9e71124221bf06cf511e31774e9cea8520e82de7
TAK848 <pairs_char0j@icloud.com>
2023-01-25 10:57:51 +0900

    static

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 set -e
 
-if [ -n "$1" ]; then
-    exec "$@"
-fi
-
 python manage.py makemigrations
 python manage.py migrate
+python manage.py collectstatic --noinput
 
 exec uwsgi --ini ./uwsgi.ini
